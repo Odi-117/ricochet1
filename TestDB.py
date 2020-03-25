778d3b2fbfee516c6d50419b968d36af76f16ed0
Odi-117 <43498421+Odi-117@users.noreply.github.com>
2020-03-25 16:14:07 +0200

    Delete TestDB.py

--- TestDB.py
+++
@@ -1,7 +0,0 @@
-from DBConnect import DBConnect 
-
-dbUrl = {"dbname":"test3","user":"postgres","password":"5225", "host":"localhost"}
-
-db = DBConnect(dbUrl,  "users")
-
-print(db.req_select_db("*",""))
